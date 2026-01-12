e85a9d4170abaa1b5c89e3ed016493c737fed2e4
JesseLeeOnFB <Skeeterjeeter8@gmail.com>
2026-01-12 07:51:26 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,67 +1,51 @@
-// Import Firebase modules
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } 
-  from "https://www.gstatic.com/firebasejs/12.7.0/firebase-auth.js";
-import { getStorage, ref as storageRef, uploadBytes } 
-  from "https://www.gstatic.com/firebasejs/12.7.0/firebase-storage.js";
+// Use your credentials
+const CLIENT_ID = "865490150118-ti166sdlmjgqrcd552ttigauldt36ik5.apps.googleusercontent.com";
+const API_KEY = "AIzaSyCkHCYkSmQvRE84DrwfPqwUrOGjpS_ItFs";
 
-// Firebase config
-const firebaseConfig = {
-  apiKey: "AIzaSyCJyO1S8WQqnfjl2JC8aE63fd_q2RDS3fw",
-  authDomain: "ohio-realtors-beta.firebaseapp.com",
-  projectId: "ohio-realtors-beta",
-  storageBucket: "ohio-realtors-beta.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
-};
+function gapiLoaded() {
+  gapi.load('client:auth2', initializeGapiClient);
+}
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const storage = getStorage(app);
+async function initializeGapiClient() {
+  await gapi.client.init({
+    apiKey: API_KEY,
+    discoveryDocs: ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"]
+  });
+  gapi.auth2.init({ client_id: CLIENT_ID });
+  console.log("GAPI client initialized");
+}
 
-// HTML Elements
-const emailInput = document.getElementById("emailInput");
-const passwordInput = document.getElementById("passwordInput");
-const registerBtn = document.getElementById("registerBtn");
-const loginBtn = document.getElementById("loginBtn");
-const authStatus = document.getElementById("authStatus");
-const authDiv = document.getElementById("authDiv");
-const appDiv = document.getElementById("appDiv");
-const uploadBtn = document.getElementById("uploadBtn");
-const pdfUpload = document.getElementById("pdfUpload");
-const uploadStatus = document.getElementById("uploadStatus");
-
-// Register
-registerBtn.addEventListener("click", () => {
-  createUserWithEmailAndPassword(auth, emailInput.value, passwordInput.value)
-    .then(userCredential => {
-      authDiv.style.display = "none";
-      appDiv.style.display = "block";
-      authStatus.innerText = "";
-    })
-    .catch(err => authStatus.innerText = err.message);
+document.getElementById('loginBtn').addEventListener('click', async () => {
+  const authInstance = gapi.auth2.getAuthInstance();
+  await authInstance.signIn();
+  document.getElementById('status').innerText = "Logged in! You can now upload PDFs.";
 });
 
-// Login
-loginBtn.addEventListener("click", () => {
-  signInWithEmailAndPassword(auth, emailInput.value, passwordInput.value)
-    .then(userCredential => {
-      authDiv.style.display = "none";
-      appDiv.style.display = "block";
-      authStatus.innerText = "";
-    })
-    .catch(err => authStatus.innerText = err.message);
-});
+document.getElementById('uploadBtn').addEventListener('click', async () => {
+  const files = document.getElementById('pdfInput').files;
+  if (!files.length) return alert("Select PDF(s) first!");
 
-// Upload PDFs
-uploadBtn.addEventListener("click", async () => {
-  const user = auth.currentUser;
-  if (!user) return alert("Log in first");
-  const files = pdfUpload.files;
+  const accessToken = gapi.auth2.getAuthInstance().currentUser.get().getAuthResponse().access_token;
+
   for (let file of files) {
-    const fileRef = storageRef(storage, `${user.uid}/${file.name}`);
-    await uploadBytes(fileRef, file);
+    const metadata = { name: file.name, mimeType: file.type };
+    const form = new FormData();
+    form.append('metadata', new Blob([JSON.stringify(metadata)], { type: 'application/json' }));
+    form.append('file', file);
+
+    const res = await fetch(
+      "https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart&fields=id",
+      {
+        method: "POST",
+        headers: { "Authorization": "Bearer " + accessToken },
+        body: form
+      }
+    );
+    const data = await res.json();
+    console.log("Uploaded:", file.name, "ID:", data.id);
   }
-  uploadStatus.innerText = "Templates uploaded!";
+  document.getElementById('status').innerText = "PDF(s) uploaded to your Google Drive!";
 });
+
+// Load gapi
+gapiLoaded();
